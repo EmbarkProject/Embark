e85e89e1cd0fe8d66202a049376f82e6c97a3b97
Lina Breslav <linabreslav@gmail.com>
2017-01-05 16:10:38 -0500

    testing a change on the new repo

---
+++ Embark/embarkapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Embarker(models.Model):
+    industryPrefs = models.CharField(default='', max_length=255)
+    culturePrefs = models.CharField(default='', max_length=255)
+    location = models.CharField(default='', max_length=255)
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
