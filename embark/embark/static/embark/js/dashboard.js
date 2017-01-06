afc12b4a10cc97b4bd9dda1814580457c97269e9
John D. Bower <jdbower0525@gmail.com>
2017-01-06 11:25:43 -0500

    removing multiples

--- embark/embark/static/embark/js/dashboard.js
+++
@@ -1,4 +0,0 @@
-function js_test() {
-    console.log("JS is linked correctly!")
-}
-js_test()
--- embark/embarkapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmbarkappConfig(AppConfig):
-    name = 'embarkapp'
