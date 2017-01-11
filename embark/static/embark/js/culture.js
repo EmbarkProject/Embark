93733172a9c9e7b7af20beccfe55209f15c0c3bc
Sanchith Kuttappa <sanchith@gmail.com>
2017-01-11 10:45:17 -0500

    cleaned up culture.js

--- embark/static/embark/js/culture.js
+++ embark/static/embark/js/culture.js
@@ -1,39 +1,3 @@
-// setting up the industry values
-var industry = {
-    "1" : "Art,Entertainment",
-    "2" : "Beauty,Wellness",
-    "3" : "Education",
-    "4" : "General Labor",
-    "5" : "Hospitality",
-    "6" : "Maintenance,Repair",
-    "7" : "Medical,Healthcare",
-    "8" : "Restaurant",
-    "9" : "Retail",
-    "10" : "Sales,Customer Service",
-    "11" : "Security",
-    "12" : "Skilled Trade",
-    "13" : "Fitness",
-    "14" : "Transportation",
-    "15" : "Administrative",
-    "16" : "Accounting",
-    "17" : "Analyst",
-    "18" : "Architecture",
-    "19" : "Insurance",
-    "20" : "Business Development",
-    "21" : "Engineering",
-    "22" : "Human Resources",
-    "23" : "Legal",
-    "24" : "Construction",
-    "25" : "Advertising",
-    "26" : "Non-Profit",
-    "27" : "Management",
-    "28" : "Real Estate",
-    "29" : "Science",
-    "30" : "Senior Management",
-    "31" : "IT",
-    "32" : "Writing"
-}
-
 // company culture slider
 $( function() {
   $( "#slider-range-max" ).slider({
