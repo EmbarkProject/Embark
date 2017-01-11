2639769bdd5805be89522c601c743165b109770a
Sanchith Kuttappa <sanchith@gmail.com>
2017-01-11 15:27:45 -0500

    added heat map to the industries page

--- embark/static/embark/js/industries.js
+++ embark/static/embark/js/industries.js
@@ -1,4 +1,72 @@
-function js_test() {
-    console.log("JS is linked correctly!")
-}
-js_test()
+// function js_test() {
+//     console.log("JS is linked correctly!")
+// }
+// js_test()
+
+$(function () {
+
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=us-population-density.json&callback=?', function (data) {
+
+        // Make codes uppercase to match the map data
+        $.each(data, function () {
+            this.code = this.code.toUpperCase();
+        });
+
+        // Instanciate the map
+        Highcharts.mapChart('container', {
+
+            chart: {
+                borderWidth: 1,
+                backgroundColor: '#ededed',
+                borderWidth: 0
+            },
+
+            title: {
+                text: 'US industry job density (/km²)'
+            },
+
+            legend: {
+                layout: 'horizontal',
+                borderWidth: 0,
+                backgroundColor: '#ededed',
+                floating: true,
+                verticalAlign: 'top',
+                y: 25
+            },
+
+            mapNavigation: {
+                enabled: true
+            },
+
+            colorAxis: {
+                min: 1,
+                type: 'logarithmic',
+                minColor: '#ffffff',
+                maxColor: '#000000',
+                stops: [
+                    [0, '#ffffff'],
+                    [0.67, '#ff5a5f'],
+                    [1, '#3d1517']
+                ]
+            },
+
+            series: [{
+                animation: {
+                    duration: 1000
+                },
+                data: data,
+                mapData: Highcharts.maps['countries/us/us-all'],
+                joinBy: ['postal-code', 'code'],
+                dataLabels: {
+                    enabled: true,
+                    color: '#FFFFFF',
+                    format: '{point.code}'
+                },
+                name: 'Population density',
+                tooltip: {
+                    pointFormat: '{point.code}: {point.value}/km²'
+                }
+            }]
+        });
+    });
+});
