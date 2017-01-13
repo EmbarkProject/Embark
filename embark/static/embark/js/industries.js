37f337df128d80fe8b83914eeb1e79a3f3203326
Sanchith Kuttappa <sanchith@gmail.com>
2017-01-13 11:54:47 -0500

    updated the heat map to show sifferent colors for the 3 diff industries

--- embark/static/embark/js/industries.js
+++ embark/static/embark/js/industries.js
@@ -1,4 +1,4 @@
-
+// industry1 heat map
 $(function () {
 
     $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=us-population-density.json&callback=?', function (data) {
@@ -9,10 +9,9 @@ $(function () {
         });
 
         // Instanciate the map
-        Highcharts.mapChart('container', {
+        Highcharts.mapChart('container1', {
 
             chart: {
-                borderWidth: 1,
                 backgroundColor: '#ededed',
                 borderWidth: 0
             },
@@ -38,7 +37,7 @@ $(function () {
                 min: 1,
                 type: 'logarithmic',
                 minColor: '#ffffff',
-                maxColor: '#000000',
+                maxColor: '#3d1517',
                 stops: [
                     [0, '#ffffff'],
                     [0.67, '#ff5a5f'],
@@ -65,4 +64,160 @@ $(function () {
             }]
         });
     });
+});
+
+// industry2 heat map
+$(function () {
+
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=us-population-density.json&callback=?', function (data) {
+
+        // Make codes uppercase to match the map data
+        $.each(data, function () {
+            this.code = this.code.toUpperCase();
+        });
+
+        // Instanciate the map
+        Highcharts.mapChart('container2', {
+
+            chart: {
+                backgroundColor: '#ededed',
+                borderWidth: 0
+            },
+
+            title: {
+                text: 'US industry job density (/km²)'
+            },
+
+            legend: {
+                layout: 'horizontal',
+                borderWidth: 0,
+                backgroundColor: '#ededed',
+                floating: true,
+                verticalAlign: 'top',
+                y: 25
+            },
+
+            mapNavigation: {
+                enabled: true
+            },
+
+            colorAxis: {
+                min: 1,
+                type: 'logarithmic',
+                minColor: '#ffffff',
+                maxColor: '#01141E',
+                stops: [
+                    [0, '#ffffff'],
+                    [0.67, '#5299D3'],
+                    [1, '#01141E']
+                ]
+            },
+
+            series: [{
+                animation: {
+                    duration: 1000
+                },
+                data: data,
+                mapData: Highcharts.maps['countries/us/us-all'],
+                joinBy: ['postal-code', 'code'],
+                dataLabels: {
+                    enabled: true,
+                    color: '#FFFFFF',
+                    format: '{point.code}'
+                },
+                name: 'Population density',
+                tooltip: {
+                    pointFormat: '{point.code}: {point.value}/km²'
+                }
+            }]
+        });
+    });
+});
+
+// industry3 heat map
+$(function () {
+
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=us-population-density.json&callback=?', function (data) {
+
+        // Make codes uppercase to match the map data
+        $.each(data, function () {
+            this.code = this.code.toUpperCase();
+        });
+
+        // Instanciate the map
+        Highcharts.mapChart('container3', {
+
+            chart: {
+                backgroundColor: '#ededed',
+                borderWidth: 0
+            },
+
+            title: {
+                text: 'US industry job density (/km²)'
+            },
+
+            legend: {
+                layout: 'horizontal',
+                borderWidth: 0,
+                backgroundColor: '#ededed',
+                floating: true,
+                verticalAlign: 'top',
+                y: 25
+            },
+
+            mapNavigation: {
+                enabled: true
+            },
+
+            colorAxis: {
+                min: 1,
+                type: 'logarithmic',
+                minColor: '#ffffff',
+                maxColor: '#2B303A',
+                stops: [
+                    [0, '#ffffff'],
+                    [0.67, '#767676'],
+                    [1, '#2B303A']
+                ]
+            },
+
+            series: [{
+                animation: {
+                    duration: 1000
+                },
+                data: data,
+                mapData: Highcharts.maps['countries/us/us-all'],
+                joinBy: ['postal-code', 'code'],
+                dataLabels: {
+                    enabled: true,
+                    color: '#FFFFFF',
+                    format: '{point.code}'
+                },
+                name: 'Population density',
+                tooltip: {
+                    pointFormat: '{point.code}: {point.value}/km²'
+                }
+            }]
+        });
+    });
+});
+
+$(document).ready(function(event){
+  $("#container2").hide();
+  $("#container3").hide();
+  $(".industryItemCoral").click(function() {
+    $("#container1").show();
+    $("#container2").hide();
+    $("#container3").hide();
+  });
+  $(".industryItemSky").click(function() {
+    $("#container2").show();
+    $("#container1").hide();
+    $("#container3").hide();
+  });
+  $(".industryItemCharcoal").click(function() {
+    $("#container3").show();
+    $("#container1").hide();
+    $("#container2").hide();
+  });
 });
