0ccbe21f54b7ce350a534f60dac97c6018631d5b
John D. Bower <jdbower0525@gmail.com>
2017-01-14 18:57:26 -0500

    got the quiz working

--- embark/static/embark/js/quiz.js
+++ embark/static/embark/js/quiz.js
@@ -75,89 +75,291 @@ function quizredirect(){
     url = '/embark/industries'
     window.location = url;
 }
-
+$("#ques9").hide();
 // Quiz flow - industry results selection
 $(document).ready(function(event) {
-
 $("#quizButton1").click(function() {
-    if ($("input[id=q1a]:checked").val()) {
-      console.log('q1a')
-      $("#ques2a").show();
-      $("#ques1").hide();
-      $("#ques2b").hide();
-      $("#ques3").hide();
-      $("#ques4").hide();
-      $("#ques5").hide();
-      $("#ques6").hide();
-      $("#ques7").hide();
-      $("#ques8").hide();
-    } else if ($("input[id=q1b]:checked").val())  {
-      console.log('q1b')
-      $("#ques2b").show();
-      $("#ques1").hide();
-      $("#ques2a").hide();
-      $("#ques3").hide();
-      $("#ques4").hide();
-      $("#ques5").hide();
-      $("#ques6").hide();
-      $("#ques7").hide();
-      $("#ques8").hide();
+    if ($("input[id=q1a]:checked").val()){
+    $("#ques2").show();
+    $("#ques1").hide();
+    } else if ($("input[id=q1b]:checked").val()){
+    $("#ques3").show();
+    $("#ques1").hide();
     }
-  });
+});
 
-  $("#quizButton2a").click(function() {
-      if ($("input[id=q2a1]:checked").val()) {
-        console.log('q2a1')
-        $("#ques6").show();
-        $("#ques1").hide();
-        $("#ques2a").hide();
-        $("#ques2b").hide();
-        $("#ques3").hide();
-        $("#ques4").hide();
-        $("#ques5").hide();
-        $("#ques7").hide();
-        $("#ques8").hide();
-      } else if ($("input[id=q2a2]:checked").val())  {
-        console.log('q2a2')
-        $("#ques8").show();
-        $("#ques1").hide();
-        $("#ques2a").hide();
-        $("#ques2b").hide();
-        $("#ques3").hide();
-        $("#ques4").hide();
-        $("#ques5").hide();
-        $("#ques6").hide();
-        $("#ques7").hide();
-      }
-    });
+$("#quizButton2").click(function() {
+    if ($("input[id=q2a]:checked").val()) {
+    $("#ques6").hide();
+    $("#ques1").hide();
+    $("#ques2").hide();
+    $("#ques3").hide();
+    $("#ques3").hide();
+    $("#ques4").hide();
+    $("#ques5").hide();
+    $("#ques7").show();
+    $("#ques8").hide();
+    $("#ques9").hide();
+    } else if ($("input[id=q2b]:checked").val())  {
+    $("#ques9").show();
+    $("#ques1").hide();
+    $("#ques2").hide();
+    $("#ques3").hide();
+    $("#ques4").hide();
+    $("#ques5").hide();
+    $("#ques6").hide();
+    $("#ques7").hide();
+    $("#ques8").hide();
+    }
+});
 
-    $("#quizButton2b").click(function() {
-        if ($("input[id=q2b1]:checked").val()) {
-          console.log('q2b1')
-          $("#ques3").show();
-          $("#ques1").hide();
-          $("#ques2a").hide();
-          $("#ques2b").hide();
-          $("#ques4").hide();
-          $("#ques5").hide();
-          $("#ques6").hide();
-          $("#ques7").hide();
-          $("#ques8").hide();
-        } else if ($("input[id=q2b2]:checked").val())  {
-          console.log('q2b2')
-          $("#ques5").show();
-          $("#ques1").hide();
-          $("#ques2a").hide();
-          $("#ques2b").hide();
-          $("#ques3").hide();
-          $("#ques4").hide();
-          $("#ques6").hide();
-          $("#ques7").hide();
-          $("#ques8").hide();
+$("#quizButton3").click(function() {
+    if ($("input[id=q3a]:checked").val()) {
+    $("#ques4").show();
+    $("#ques1").hide();
+    $("#ques2").hide();
+    $("#ques3").hide();
+    $("#ques5").hide();
+    $("#ques6").hide();
+    $("#ques7").hide();
+    $("#ques8").hide();
+    $("#ques9").hide();
+    } else if ($("input[id=q3b]:checked").val())  {
+    $("#ques6").show();
+    $("#ques1").hide();
+    $("#ques2").hide();
+    $("#ques3").hide();
+    $("#ques4").hide();
+    $("#ques5").hide();
+    $("#ques7").hide();
+    $("#ques8").hide();
+    $("#ques9").hide();
+    }
+});
+
+$("#quizButton4").click(function() {
+    if ($("input[id=q4a]:checked").val()) {
+    $("#ques4").hide();
+    $("#ques1").hide();
+    $("#ques2").hide();
+    $("#ques3").hide();
+    $("#ques5").show();
+    $("#ques6").hide();
+    $("#ques7").hide();
+    $("#ques8").hide();
+    $("#ques9").hide();
+    } else if ($("input[id=q4b]:checked").val()){
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['14,3,13'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
         }
-      });
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    }
+});
 
+$("#quizButton5").click(function() {
+    if ($("input[id=q5a]:checked").val()) {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['8,9,10'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    } else if ($("input[id=q5b]:checked").val())  {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['2,5,7'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    }
+});
 
+$("#quizButton6").click(function() {
+    if ($("input[id=q6a]:checked").val()) {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['1,12,24'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    } else if ($("input[id=q6b]:checked").val())  {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['4,6,11'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    }
+});
+
+$("#quizButton7").click(function(){
+    if ($("input[id=q7a]:checked").val()){
+    $("#ques4").hide();
+    $("#ques1").hide();
+    $("#ques2").hide();
+    $("#ques3").hide();
+    $("#ques5").hide();
+    $("#ques6").hide();
+    $("#ques7").hide();
+    $("#ques8").show();
+    $("#ques9").hide();
+    } else if ($("input[id=q7b]:checked").val()){
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['26,15,19'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    }
+});
+
+$("#quizButton8").click(function() {
+    if ($("input[id=q8a]:checked").val()) {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['20,23,28'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    } else if ($("input[id=q8b]:checked").val())  {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['22,27,30'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    }
+});
+
+$("#quizButton9").click(function() {
+    if ($("input[id=q9a]:checked").val()) {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['32,25,17'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    } else if ($("input[id=q9b]:checked").val())  {
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var jobTitle = results.jobTitle
+        var ajaxdata = {
+            url: '/api/PostEmbarker/' + id + '/',
+            data: { "user": id, "industryPrefs": ['16,31,21'], "jobTitle": jobTitle},
+            dataType: 'json',
+            traditional: true,
+            type: 'PUT'
+        }
+        $.ajax(ajaxdata).done(function(results){
+            quizredirect()
+        })})
+    }
+});
 
     // else if ($("input[id=q2b]:checked").val()) {
     //   console.log('q2b')
