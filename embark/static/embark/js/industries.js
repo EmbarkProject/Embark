d63a1f0a89f4d7c5898664a86edcf78819504683
John D. Bower <jdbower0525@gmail.com>
2017-01-15 11:58:46 -0500

    got the map working

--- embark/static/embark/js/industries.js
+++ embark/static/embark/js/industries.js
@@ -1,28 +1,94 @@
 // industry1 heat map
+function get_button1(){
+    var id = document.getElementById('userId').value
+    $.ajax({
+        url: '/api/GetEmbarker/' + id + '/',
+        type: 'GET',
+        datatype: 'json',
+    }).done(function(results){
+        var industryList = results.industryPrefs.split(',');
+        var jobid = industryList[0]
+        console.log(results)
+        $.ajax({
+            url: '/api/Industry/' + jobid + '/',
+            type: 'GET',
+            datatype: 'json',
+        }).done(function(results){
+            var source = $('#post-template1').html();
+            var template = Handlebars.compile(source);
+            var html = template(results);
+            $('#industry1').append(html);
+            console.log(html)
+        })})}
+function get_button2(){
+    var id = document.getElementById('userId').value
+    $.ajax({
+        url: '/api/GetEmbarker/' + id + '/',
+        type: 'GET',
+        datatype: 'json',
+    }).done(function(results){
+        var industryList = results.industryPrefs.split(',');
+        var jobid = industryList[1]
+        $.ajax({
+            url: '/api/Industry/' + jobid + '/',
+            type: 'GET',
+            datatype: 'json',
+        }).done(function(results){
+            var source = $('#post-template2').html();
+            var template = Handlebars.compile(source);
+            var html = template(results);
+            $('#industry2').append(html);
+        })})}
+function get_button3(){
+    var id = document.getElementById('userId').value
+    $.ajax({
+        url: '/api/GetEmbarker/' + id + '/',
+        type: 'GET',
+        datatype: 'json',
+    }).done(function(results){
+        var industryList = results.industryPrefs.split(',');
+        var jobid = industryList[2]
+        $.ajax({
+            url: '/api/Industry/' + jobid + '/',
+            type: 'GET',
+            datatype: 'json',
+        }).done(function(results){
+            var source = $('#post-template3').html();
+            var template = Handlebars.compile(source);
+            var html = template(results);
+            $('#industry3').append(html);
+        })})}
+
+
+
+get_button1()
+get_button2()
+get_button3()
 $(function () {
-
     $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=us-population-density.json&callback=?', function (data) {
-
-        // Make codes uppercase to match the map data
         $.each(data, function () {
             this.code = this.code.toUpperCase();
         });
-
-        // Instanciate the map
-        var url = 'https://api.glassdoor.com/api/api.htm?t.p=112563&t.k=fKBkymF6I8W&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1'
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var industryList = results.industryPrefs.split(',')
+        var jobid = industryList[0]
+        var url = 'http://api.glassdoor.com/api/api.htm?t.p=112563&t.k=fKBkymF6I8W&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1&jc=' + jobid
         $.ajax({
             url: url,
             type: 'GET',
             dataType: 'jsonp',
         }).done(function(results){
-            console.log(results)
+            state = results.response.states
         Highcharts.mapChart('container1', {
-
             chart: {
                 backgroundColor: '#ededed',
                 borderWidth: 0
             },
-
             title: {
                 text: 'US industry job density (/km²)'
             },
@@ -47,7 +113,7 @@ $(function () {
                 maxColor: '#3d1517',
                 stops: [
                     [0, '#ffffff'],
-                    [0.67, '#ff5a5f'],
+                    [0.60, '#ff5a5f'],
                     [1, '#3d1517']
                 ]
             },
@@ -56,7 +122,57 @@ $(function () {
                 animation: {
                     duration: 1000
                 },
-                data: data,
+                data: [{code: "AL", value:state["Alabama"].numJobs},
+                       {code: "AK", value:state["Alaska"].numJobs},
+                       {code: "AZ", value:state["Arizona"].numJobs},
+                       {code: "AR", value:state["Arkansas"].numJobs},
+                       {code: "CA", value:state["California"].numJobs},
+                       {code: "CO", value:state["Colorado"].numJobs},
+                       {code: "CT", value:state["Connecticut"].numJobs},
+                       {code: "DE", value:state["Delaware"].numJobs},
+                       {code: "FL", value:state["Florida"].numJobs},
+                       {code: "GA", value:state["Georgia"].numJobs},
+                       {code: "HI", value:state["Hawaii"].numJobs},
+                       {code: "ID", value:state["Idaho"].numJobs},
+                       {code: "IL", value:state["Illinois"].numJobs},
+                       {code: "IN", value:state["Indiana"].numJobs},
+                       {code: "IA", value:state["Iowa"].numJobs},
+                       {code: "KS", value:state["Kansas"].numJobs},
+                       {code: "KY", value:state["Kentucky"].numJobs},
+                       {code: "LA", value:state["Louisiana"].numJobs},
+                       {code: "ME", value:state["Maine"].numJobs},
+                       {code: "MD", value:state["Maryland"].numJobs},
+                       {code: "MA", value:state["Massachusetts"].numJobs},
+                       {code: "MI", value:state["Michigan"].numJobs},
+                       {code: "MN", value:state["Minnesota"].numJobs},
+                       {code: "MS", value:state["Mississippi"].numJobs},
+                       {code: "MO", value:state["Missouri"].numJobs},
+                       {code: "MT", value:state["Montana"].numJobs},
+                       {code: "NE", value:state["Nebraska"].numJobs},
+                       {code: "NV", value:state["Nevada"].numJobs},
+                       {code: "NH", value:state["New Hampshire"].numJobs},
+                       {code: "NJ", value:state["New Jersey"].numJobs},
+                       {code: "NM", value:state["New Mexico"].numJobs},
+                       {code: "NY", value:state["New York"].numJobs},
+                       {code: "NC", value:state["North Carolina"].numJobs},
+                       {code: "ND", value:state["North Dakota"].numJobs},
+                       {code: "OH", value:state["Ohio"].numJobs},
+                       {code: "OK", value:state["Oklahoma"].numJobs},
+                       {code: "OR", value:state["Oregon"].numJobs},
+                       {code: "PA", value:state["Pennsylvania"].numJobs},
+                       {code: "RI", value:state["Rhode Island"].numJobs},
+                       {code: "SC", value:state["South Carolina"].numJobs},
+                       {code: "SD", value:state["South Dakota"].numJobs},
+                       {code: "TN", value:state["Tennessee"].numJobs},
+                       {code: "TX", value:state["Texas"].numJobs},
+                       {code: "UT", value:state["Utah"].numJobs},
+                       {code: "VT", value:state["Vermont"].numJobs},
+                       {code: "VA", value:state["Virginia"].numJobs},
+                       {code: "WA", value:state["Virginia"].numJobs},
+                       {code: "WV", value:state["West Virginia"].numJobs},
+                       {code: "WI", value:state["Wisconsin"].numJobs},
+                       {code: "WY", value:state["Wyoming"].numJobs}
+                ],
                 mapData: Highcharts.maps['countries/us/us-all'],
                 joinBy: ['postal-code', 'code'],
                 dataLabels: {
@@ -64,14 +180,14 @@ $(function () {
                     color: '#FFFFFF',
                     format: '{point.code}'
                 },
-                name: 'Population density',
+                name: 'Job Distribution',
                 tooltip: {
                     pointFormat: '{point.code}: {point.value}/km²'
                 }
             }]
         });
     })});
-});
+})});
 
 // industry2 heat map
 $(function () {
@@ -82,7 +198,21 @@ $(function () {
         $.each(data, function () {
             this.code = this.code.toUpperCase();
         });
-
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var industryList = results.industryPrefs.split(',')
+        var jobid = industryList[1]
+        var url = 'http://api.glassdoor.com/api/api.htm?t.p=112563&t.k=fKBkymF6I8W&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1&jc=' + jobid
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'jsonp',
+        }).done(function(results){
+            state = results.response.states
         // Instanciate the map
         Highcharts.mapChart('container2', {
 
@@ -124,7 +254,57 @@ $(function () {
                 animation: {
                     duration: 1000
                 },
-                data: data,
+                data: [{code: "AL", value:state["Alabama"].numJobs},
+                       {code: "AK", value:state["Alaska"].numJobs},
+                       {code: "AZ", value:state["Arizona"].numJobs},
+                       {code: "AR", value:state["Arkansas"].numJobs},
+                       {code: "CA", value:state["California"].numJobs},
+                       {code: "CO", value:state["Colorado"].numJobs},
+                       {code: "CT", value:state["Connecticut"].numJobs},
+                       {code: "DE", value:state["Delaware"].numJobs},
+                       {code: "FL", value:state["Florida"].numJobs},
+                       {code: "GA", value:state["Georgia"].numJobs},
+                       {code: "HI", value:state["Hawaii"].numJobs},
+                       {code: "ID", value:state["Idaho"].numJobs},
+                       {code: "IL", value:state["Illinois"].numJobs},
+                       {code: "IN", value:state["Indiana"].numJobs},
+                       {code: "IA", value:state["Iowa"].numJobs},
+                       {code: "KS", value:state["Kansas"].numJobs},
+                       {code: "KY", value:state["Kentucky"].numJobs},
+                       {code: "LA", value:state["Louisiana"].numJobs},
+                       {code: "ME", value:state["Maine"].numJobs},
+                       {code: "MD", value:state["Maryland"].numJobs},
+                       {code: "MA", value:state["Massachusetts"].numJobs},
+                       {code: "MI", value:state["Michigan"].numJobs},
+                       {code: "MN", value:state["Minnesota"].numJobs},
+                       {code: "MS", value:state["Mississippi"].numJobs},
+                       {code: "MO", value:state["Missouri"].numJobs},
+                       {code: "MT", value:state["Montana"].numJobs},
+                       {code: "NE", value:state["Nebraska"].numJobs},
+                       {code: "NV", value:state["Nevada"].numJobs},
+                       {code: "NH", value:state["New Hampshire"].numJobs},
+                       {code: "NJ", value:state["New Jersey"].numJobs},
+                       {code: "NM", value:state["New Mexico"].numJobs},
+                       {code: "NY", value:state["New York"].numJobs},
+                       {code: "NC", value:state["North Carolina"].numJobs},
+                       {code: "ND", value:state["North Dakota"].numJobs},
+                       {code: "OH", value:state["Ohio"].numJobs},
+                       {code: "OK", value:state["Oklahoma"].numJobs},
+                       {code: "OR", value:state["Oregon"].numJobs},
+                       {code: "PA", value:state["Pennsylvania"].numJobs},
+                       {code: "RI", value:state["Rhode Island"].numJobs},
+                       {code: "SC", value:state["South Carolina"].numJobs},
+                       {code: "SD", value:state["South Dakota"].numJobs},
+                       {code: "TN", value:state["Tennessee"].numJobs},
+                       {code: "TX", value:state["Texas"].numJobs},
+                       {code: "UT", value:state["Utah"].numJobs},
+                       {code: "VT", value:state["Vermont"].numJobs},
+                       {code: "VA", value:state["Virginia"].numJobs},
+                       {code: "WA", value:state["Virginia"].numJobs},
+                       {code: "WV", value:state["West Virginia"].numJobs},
+                       {code: "WI", value:state["Wisconsin"].numJobs},
+                       {code: "WY", value:state["Wyoming"].numJobs}
+                ],
                 mapData: Highcharts.maps['countries/us/us-all'],
                 joinBy: ['postal-code', 'code'],
                 dataLabels: {
@@ -139,8 +319,8 @@ $(function () {
                 }
             }]
         });
-    });
-});
+    })});
+})});
 
 // industry3 heat map
 $(function () {
@@ -151,7 +331,21 @@ $(function () {
         $.each(data, function () {
             this.code = this.code.toUpperCase();
         });
-
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var industryList = results.industryPrefs.split(',')
+        var jobid = industryList[2]
+        var url = 'http://api.glassdoor.com/api/api.htm?t.p=112563&t.k=fKBkymF6I8W&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1&jc=' + jobid
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'jsonp',
+        }).done(function(results){
+            state = results.response.states
         // Instanciate the map
         Highcharts.mapChart('container3', {
 
@@ -193,7 +387,57 @@ $(function () {
                 animation: {
                     duration: 1000
                 },
-                data: data,
+                data: [{code: "AL", value:state["Alabama"].numJobs},
+                       {code: "AK", value:state["Alaska"].numJobs},
+                       {code: "AZ", value:state["Arizona"].numJobs},
+                       {code: "AR", value:state["Arkansas"].numJobs},
+                       {code: "CA", value:state["California"].numJobs},
+                       {code: "CO", value:state["Colorado"].numJobs},
+                       {code: "CT", value:state["Connecticut"].numJobs},
+                       {code: "DE", value:state["Delaware"].numJobs},
+                       {code: "FL", value:state["Florida"].numJobs},
+                       {code: "GA", value:state["Georgia"].numJobs},
+                       {code: "HI", value:state["Hawaii"].numJobs},
+                       {code: "ID", value:state["Idaho"].numJobs},
+                       {code: "IL", value:state["Illinois"].numJobs},
+                       {code: "IN", value:state["Indiana"].numJobs},
+                       {code: "IA", value:state["Iowa"].numJobs},
+                       {code: "KS", value:state["Kansas"].numJobs},
+                       {code: "KY", value:state["Kentucky"].numJobs},
+                       {code: "LA", value:state["Louisiana"].numJobs},
+                       {code: "ME", value:state["Maine"].numJobs},
+                       {code: "MD", value:state["Maryland"].numJobs},
+                       {code: "MA", value:state["Massachusetts"].numJobs},
+                       {code: "MI", value:state["Michigan"].numJobs},
+                       {code: "MN", value:state["Minnesota"].numJobs},
+                       {code: "MS", value:state["Mississippi"].numJobs},
+                       {code: "MO", value:state["Missouri"].numJobs},
+                       {code: "MT", value:state["Montana"].numJobs},
+                       {code: "NE", value:state["Nebraska"].numJobs},
+                       {code: "NV", value:state["Nevada"].numJobs},
+                       {code: "NH", value:state["New Hampshire"].numJobs},
+                       {code: "NJ", value:state["New Jersey"].numJobs},
+                       {code: "NM", value:state["New Mexico"].numJobs},
+                       {code: "NY", value:state["New York"].numJobs},
+                       {code: "NC", value:state["North Carolina"].numJobs},
+                       {code: "ND", value:state["North Dakota"].numJobs},
+                       {code: "OH", value:state["Ohio"].numJobs},
+                       {code: "OK", value:state["Oklahoma"].numJobs},
+                       {code: "OR", value:state["Oregon"].numJobs},
+                       {code: "PA", value:state["Pennsylvania"].numJobs},
+                       {code: "RI", value:state["Rhode Island"].numJobs},
+                       {code: "SC", value:state["South Carolina"].numJobs},
+                       {code: "SD", value:state["South Dakota"].numJobs},
+                       {code: "TN", value:state["Tennessee"].numJobs},
+                       {code: "TX", value:state["Texas"].numJobs},
+                       {code: "UT", value:state["Utah"].numJobs},
+                       {code: "VT", value:state["Vermont"].numJobs},
+                       {code: "VA", value:state["Virginia"].numJobs},
+                       {code: "WA", value:state["Virginia"].numJobs},
+                       {code: "WV", value:state["West Virginia"].numJobs},
+                       {code: "WI", value:state["Wisconsin"].numJobs},
+                       {code: "WY", value:state["Wyoming"].numJobs}
+                ],
                 mapData: Highcharts.maps['countries/us/us-all'],
                 joinBy: ['postal-code', 'code'],
                 dataLabels: {
@@ -207,8 +451,8 @@ $(function () {
                 }
             }]
         });
-    });
-});
+    })});
+})});
 
 // selecting an industry makes the corresponding map show
 $(document).ready(function(event){
@@ -229,24 +473,21 @@ $(document).ready(function(event){
     $(".industryItemCharcoal").css("background-color", "white");
     $(".iconCharcoal").css("color", "#2B303A");
     $(".industryTextCharcoal").css("color", "#2B303A");
-  });
+});
 
-  $(".industryItemSky").click(function() {
+$(".industryItemSky").click(function() {
     $("#container2").show();
     $("#container1").hide();
     $("#container3").hide();
     $(".industryItemSky").css("background-color", "#5299D3");
     $(".iconSky").css("color", "white");
     $(".industryTextSky").css("color", "white");
-
     $(".industryItemCoral").css("background-color", "white");
     $(".iconCoral").css("color", "#ff5A5F");
     $(".industryTextCoral").css("color", "#ff5A5F");
-
     $(".industryItemCharcoal").css("background-color", "white");
     $(".iconCharcoal").css("color", "#2B303A");
     $(".industryTextCharcoal").css("color", "#2B303A");
-
   });
   $(".industryItemCharcoal").click(function() {
     $("#container3").show();
