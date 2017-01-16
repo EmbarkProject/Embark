bc405159b225fdecc0fc2dc9a8153492cc36feaa
John D. Bower <jdbower0525@gmail.com>
2017-01-16 11:18:30 -0500

    monday morning commit

--- embark/static/embark/js/dashboard.js
+++ embark/static/embark/js/dashboard.js
@@ -15,21 +15,22 @@ $(function () {
             dataType: 'jsonp'
         }).done(function(jobs){
             var jobs = jobs.response.results
-            Highcharts.chart('chartContainer', {
+            Highcharts.chart('chartContainer1', {
                 chart: {
-                    backgroundColor: '#ededed',
+                    backgroundColor: '#2B303A',
                     plotBackgroundColor: null,
                     plotBorderWidth: 0,
-                    plotShadow: false
+                    plotShadow: false,
+                    spacingTop: 10,
                 },
                 exporting: { enabled: false },
                 title: {
                     text: jobTitle.toUpperCase().bold(),
                     align: 'center',
                     verticalAlign: 'top',
-                    y: 300,
+                    y: 50,
                     style: {
-                        color: '#2B303A',
+                        color: '#5299d3',
                         fontSize:'25px'
                     }
                 },
@@ -41,12 +42,12 @@ $(function () {
                         dataLabels: {
                             borderWidth: 0,
                             enabled: true,
-                            distance: 0,
+                            distance: -12,
                             style: {
                                 fontWeight: 'bold',
-                                color: '#2B303A',
+                                color: 'white',
                                 fontFamily: '$font',
-                                fontSize: 15
+                                fontSize: 10
                             }
                         },
                         startAngle: -90,
@@ -57,6 +58,7 @@ $(function () {
                 series: [{
                     type: 'pie',
                     name: 'Distribution',
+                    size: 375,
                     innerSize: '50%',
                     data: [
                         [jobs[0].nextJobTitle.toUpperCase(), jobs[0].frequencyPercent],
@@ -78,9 +80,128 @@ $(function () {
     })
 });
 
+$(function () {
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=us-population-density.json&callback=?', function (data) {
+        $.each(data, function () {
+            this.code = this.code.toUpperCase();
+        });
+        id = document.getElementById('userId').value
+        var url = '/api/GetEmbarker/' + id + '/'
+        $.ajax({
+            url: url,
+            type: 'GET',
+        }).done(function(results){
+        var industryList = results.industryPrefs.split(',')
+        var jobid = industryList[0]
+        var url = 'http://api.glassdoor.com/api/api.htm?t.p=112563&t.k=fKBkymF6I8W&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1&jc=' + jobid
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'jsonp',
+        }).done(function(results){
+            state = results.response.states
+            console.log(state)
+        Highcharts.mapChart('dashboardmap', {
+            chart: {
+                backgroundColor: '#ededed',
+                borderWidth: 0
+            },
+            title: {
+                text: 'US industry job density'
+            },
 
+            legend: {
+                layout: 'horizontal',
+                borderWidth: 0,
+                backgroundColor: '#ededed',
+                floating: true,
+                verticalAlign: 'top',
+                y: 25
+            },
 
-function js_test() {
-    console.log("JS is linked correctly!")
-}
-js_test()
+            mapNavigation: {
+                enabled: true
+            },
+
+            colorAxis: {
+                min: 1,
+                type: 'logarithmic',
+                minColor: '#ffffff',
+                maxColor: '#3d1517',
+                stops: [
+                    [0, '#ffffff'],
+                    [0.50, '#ff5a5f'],
+                    [1, '#3d1517']
+                ]
+            },
+
+            series: [{
+                animation: {
+                    duration: 1000
+                },
+                data: [{code: "AL", value:state["Alabama"].score},
+                       {code: "AK", value:state["Alaska"].score},
+                       {code: "AZ", value:state["Arizona"].score},
+                       {code: "AR", value:state["Arkansas"].score},
+                       {code: "CA", value:state["California"].score},
+                       {code: "CO", value:state["Colorado"].score},
+                       {code: "CT", value:state["Connecticut"].score},
+                       {code: "DE", value:state["Delaware"].score},
+                       {code: "FL", value:state["Florida"].score},
+                       {code: "GA", value:state["Georgia"].score},
+                       {code: "HI", value:state["Hawaii"].score},
+                       {code: "ID", value:state["Idaho"].score},
+                       {code: "IL", value:state["Illinois"].score},
+                       {code: "IN", value:state["Indiana"].score},
+                       {code: "IA", value:state["Iowa"].score},
+                       {code: "KS", value:state["Kansas"].score},
+                       {code: "KY", value:state["Kentucky"].score},
+                       {code: "LA", value:state["Louisiana"].score},
+                       {code: "ME", value:state["Maine"].score},
+                       {code: "MD", value:state["Maryland"].score},
+                       {code: "MA", value:state["Massachusetts"].score},
+                       {code: "MI", value:state["Michigan"].score},
+                       {code: "MN", value:state["Minnesota"].score},
+                       {code: "MS", value:state["Mississippi"].score},
+                       {code: "MO", value:state["Missouri"].score},
+                       {code: "MT", value:state["Montana"].score},
+                       {code: "NE", value:state["Nebraska"].score},
+                       {code: "NV", value:state["Nevada"].score},
+                       {code: "NH", value:state["New Hampshire"].score},
+                       {code: "NJ", value:state["New Jersey"].score},
+                       {code: "NM", value:state["New Mexico"].score},
+                       {code: "NY", value:state["New York"].score},
+                       {code: "NC", value:state["North Carolina"].score},
+                       {code: "ND", value:state["North Dakota"].score},
+                       {code: "OH", value:state["Ohio"].score},
+                       {code: "OK", value:state["Oklahoma"].score},
+                       {code: "OR", value:state["Oregon"].score},
+                       {code: "PA", value:state["Pennsylvania"].score},
+                       {code: "RI", value:state["Rhode Island"].score},
+                       {code: "SC", value:state["South Carolina"].score},
+                       {code: "SD", value:state["South Dakota"].score},
+                       {code: "TN", value:state["Tennessee"].score},
+                       {code: "TX", value:state["Texas"].score},
+                       {code: "UT", value:state["Utah"].score},
+                       {code: "VT", value:state["Vermont"].score},
+                       {code: "VA", value:state["Virginia"].score},
+                       {code: "WA", value:state["Virginia"].score},
+                       {code: "WV", value:state["West Virginia"].score},
+                       {code: "WI", value:state["Wisconsin"].score},
+                       {code: "WY", value:state["Wyoming"].score}
+                ],
+                mapData: Highcharts.maps['countries/us/us-all'],
+                joinBy: ['postal-code', 'code'],
+                dataLabels: {
+                    enabled: true,
+                    color: '#FFFFFF',
+                    format: '{point.code}'
+                },
+                name: 'Job Distribution',
+                tooltip: {
+                    pointFormat: '{point.code}: {point.value}'
+                }
+            }]
+        });
+    })});
+})});
--- embark/static/embark/js/jobslist.js
+++ embark/static/embark/js/jobslist.js
@@ -179,18 +179,19 @@ function filter_jobs2(city, state){
     type: 'GET',
     dataType: 'jsonp',
     }).done(function(results){
+        var cultureList = embarker.culturePrefs.split(',');
         for (var j = 0; j < results.response.employers.length; j++){
             var ratingParse = results.response.employers[j]
             var careerRating = ratingParse.careerOpportunitiesRating
-            var careerInput = embarker.culturePrefs[0]
+            var careerInput = cultureList[0]
             var cultureRating = ratingParse.cultureAndValuesRating
-            var cultureInput = embarker.culturePrefs[1]
+            var cultureInput = cultureList[1]
             var leadershipRating = ratingParse.seniorLeadershipRating
-            var leadershipInput = embarker.culturePrefs[2]
+            var leadershipInput = cultureList[2]
             var payRating = ratingParse.compensationAndBenefitsRating
-            var payInput = embarker.culturePrefs[3]
+            var payInput = cultureList[3]
             var workLifeRating = ratingParse.workLifeBalanceRating
-            var workLifeInput = embarker.culturePrefs[4]
+            var workLifeInput = cultureList[4]
             if (careerRating >= careerInput &&
                 cultureRating >= cultureInput &&
                 leadershipRating >= leadershipInput &&
@@ -199,7 +200,8 @@ function filter_jobs2(city, state){
                     var company = ratingParse.name
                     var searchcity = locationList[0][city];
                     var searchstate = locationList[0][state];
-                    var indeedurl = 'https://api.indeed.com/ads/apisearch?publisher=291337585868709&q=' + company + '&l=' + searchcity + '%2C+'+ searchstate + '&sort=&radius=&format=json&st=&jt=&start=&limit=5&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome&v=2'
+                    console.log(company)
+                    var indeedurl = 'https://api.indeed.com/ads/apisearch?publisher=291337585868709&as_and=' + company + '&l=' + searchcity + '%2C+'+ searchstate + '&sort=&radius=&format=json&st=&jt=&start=&limit=5&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome&v=2'
                     $.ajax({
                         crossOrigin: true,
                         url: indeedurl,
@@ -232,18 +234,19 @@ function filter_jobs3(city, state){
     type: 'GET',
     dataType: 'jsonp',
     }).done(function(results){
+        var cultureList = embarker.culturePrefs.split(',');
         for (var j = 0; j < results.response.employers.length; j++){
             var ratingParse = results.response.employers[j]
             var careerRating = ratingParse.careerOpportunitiesRating
-            var careerInput = embarker.culturePrefs[0]
+            var careerInput = cultureList[0]
             var cultureRating = ratingParse.cultureAndValuesRating
-            var cultureInput = embarker.culturePrefs[1]
+            var cultureInput = cultureList[1]
             var leadershipRating = ratingParse.seniorLeadershipRating
-            var leadershipInput = embarker.culturePrefs[2]
+            var leadershipInput = cultureList[2]
             var payRating = ratingParse.compensationAndBenefitsRating
-            var payInput = embarker.culturePrefs[3]
+            var payInput = cultureList[3]
             var workLifeRating = ratingParse.workLifeBalanceRating
-            var workLifeInput = embarker.culturePrefs[4]
+            var workLifeInput = cultureList[4]
             if (careerRating >= careerInput &&
                 cultureRating >= cultureInput &&
                 leadershipRating >= leadershipInput &&
